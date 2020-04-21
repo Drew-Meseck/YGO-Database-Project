e8f799ac68dd2f503d07d24af2a583c7fa74cf5a
Drew-Meseck <drootles@gmail.com>
2020-04-21 16:49:00 -0600

    Adjusted inputs and in.csv

--- Card-Insert-Generator/insert_generator.py
+++ Card-Insert-Generator/insert_generator.py
@@ -1,15 +1,20 @@
+#Drew H Meseck
+#Card Insert Generator
+#YGO-Database-Project
 
+#Takes csv of cards and returns a .txt output file of mySQL insert statements for the "Card" table. 
+#This will be used to generate other insert statements for other tables.
 
+import sys
 
-
-def generate_line(line):
-    if(line[3] == "Spell" || line[3] == "Trap"):
+def generate_line(line): # generates one line of output given some split array from the csv
+    if(line[3] == "Spell" or line[3] == "Trap"):
         ID = line[1]
         title = line[2]
         classify = line[3]
         rarity = line[4]
         price = 0
-        _type = line[7]
+        _type = line[7] 
         _Desc = line[11]
         #format write for spell/trap to fout
     else:
@@ -27,19 +32,17 @@ def generate_line(line):
         #format write for monster to fout
 
 
-
-
-def generate_insert(fin, fout):
-    for row in range(0, 3504):
+def generate_insert(fin, fout): 
+    for row in range(0, 3502):
         txt = fin.readline()
         line = txt.split('\t')
         generate_line(line)
         print(line)
 
 def main():
-   din = "C:\\Users\\droot\\Documents\\Senior Year Sem 1\\DMS\\Project\\YGO-Database-Project\\Card-Insert-Generator\\in.csv"
-   dout = "C:\\Users\\droot\\Documents\\Senior Year Sem 1\\DMS\\Project\\YGO-Database-Project\\Card-Insert-Generator\\out.txt"
-   fin = open(din, 'r')
+   din = input("Provide Input Path: ")
+   dout = input("Provide Output Path: ")
+   fin = open(din, 'r', encoding="ISO-8859-1")
    fout = open(dout, 'w')
    generate_insert(fin, fout)
 
