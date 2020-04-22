488a146107629a389507c84b0a34d625c6244b48
Drew Meseck <dm3306a@student.american.edu>
2020-04-22 12:08:05 -0600

    Finished Insert Generator

--- Card-Insert-Generator/insert_generator.py
+++ Card-Insert-Generator/insert_generator.py
@@ -10,9 +10,34 @@ import random
 
 
 def getbox(ID):
-    return "box"
+    if(len(ID) == 10):
+        return ID[0] + ID[1] + ID[2] + ID[3]
+    else:
+        return ID[0] + ID[1] + ID[2]
 
 def getPrice(rarity):
+    if(len(rarity) <= 1):
+        return str(7 + random.randint(0, 7))
+    r = rarity[0] + rarity[1]
+    r = r.lower()
+    p = 0
+    if(r == "co"):
+        p = 1
+    elif(r == "su"):
+        p = 5
+    elif (r == "ul"):
+        p = 10
+    elif (r == "se"):
+        p = 20
+    elif (r == "ra"):
+        p = 2
+    else:
+        p = 3
+    
+    rand = random.randint(0, p)
+    p = rand + p
+    return str(p)
+    
     return "$10"
 
 def generate_line(line, fout): # generates one line of output given some split array from the csv
@@ -45,6 +70,7 @@ def main():
    fin = open(din, 'r', encoding="ISO-8859-1")
    fout = open(dout, 'w', encoding="ISO-8859-1")
    generate_insert(fin, fout)
+   print("Generation Complete")
 
 
 if __name__ == '__main__':
